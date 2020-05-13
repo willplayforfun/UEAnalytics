c1a35efafc8140ba3f5dd924a886b9989caccc09
Matt Levonian (Desktop) <elevonian@gmail.com>
2020-05-13 16:26:49 -0700

    Add test lambda function

---
+++ lambda_functions/test_functions.js
@@ -0,0 +1,41 @@
+
+// When you call a serverless function’s endpoint, 
+// the handler receives an event object similar to 
+// what you would receive from the AWS API Gateway
+// {
+// 	"path": "Path parameter",
+// 	"httpMethod": "Incoming request's method name"
+// 	"headers": {Incoming request headers}
+// 	"queryStringParameters": {query string parameters }
+// 	"body": "A JSON string of the request payload."
+// 	"isBase64Encoded": "A boolean flag to indicate if the applicable request payload is Base64-encode"
+// }
+
+// The context parameter includes information about the 
+// context in which the serverless function was called, 
+// like certain Identity user information, for example.
+
+// The callback works much like the same parameter in an 
+// AWS Lambda function. Your handler should use the 
+// callback to return either an error (as the first parameter) 
+// or a response object, such as:
+// {
+// 	"isBase64Encoded": true|false,
+// 	"statusCode": httpStatusCode,
+// 	"headers": { "headerName": "headerValue", ... },
+// 	"body": "..."
+// }
+
+
+exports.handler = function(event, context, callback) {
+    // your server-side functionality
+	const subject = event.queryStringParameters.name || 'World'
+	
+	callback(
+		null, 
+		{
+			statusCode: 200,
+			body: "Hello, ${subject}!"
+		}
+	);
+}
