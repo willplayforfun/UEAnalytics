9078d9988ffe40d955aefcb2a93ac2a36c30d814
Matt Levonian (Desktop) <elevonian@gmail.com>
2020-05-13 18:02:20 -0700

    Try to fix deployed build
    Explicitly unregister serviceWorker and set homepage to '.'

--- ue-analytics/src/index.js
+++ ue-analytics/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 import './index.css';
 
 import AnalyticsDashboard from './App';
@@ -7,8 +8,7 @@ import AnalyticsDashboard from './App';
 
 ReactDOM.render(<AnalyticsDashboard />, document.getElementById('root'));
 
-// import * as serviceWorker from './serviceWorker';
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+serviceWorker.unregister();
